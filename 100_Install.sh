58e738a77faf435001d6010784dfcd2b55989a59
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-09-02 07:12:47 +0100

    Update 100_Install.sh

--- 100_Install.sh
+++ 100_Install.sh
@@ -5,7 +5,7 @@ sudo pacman -Syu
 yay -S hyprland wofi hyprpaper micro gvfs firefox timeshift grub-btrfs kitty geany geany-plugins geany-themes pcmanfm-gtk3 swaylock
 
 sudo pacman --no-confirm -S audacious copq yt-dlp pulsemixer htop calcurse lxsession playerctl lxterminal brave-bin font-manager pycharm-community-edition gthumb blueman tk python-requests python-json5 python-urllib3 python-dateutil
-sudo pacman --no-confirm -S pacman-contrib pamixer btop ttf-jetbrains-mono-nerd starship ttf-font-awesome blueman
+sudo pacman --no-confirm -S pacman-contrib pamixer btop ttf-jetbrains-mono-nerd starship ttf-font-awesome blueman xfce4-terminal lxterminal
 
 mkdir -p ~/.config/hypr
 cd hypr
